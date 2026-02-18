69330914e0e0aea1027abc53c86662946233eadf
NJI HALLE CHO-NKWENTI <njihalle10@gmail.com>
2026-02-18 20:35:48 +0000

    add the current code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useEffect, useRef } from 'react';
 
 function Hero() {
+    const videoRef = useRef();
+
+    useEffect(()=>{
+        if(videoRef.current) videoRef.current.playbackRate = 2;
+    }, []);
+
   return (
-    <div>
-      
-    </div>
+    <section id='hero'>
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src='/title.png' alt='MacBook Title'/>
+        </div>
+
+        <video ref={videoRef} src='/videos/hero.mp4' autoPlay muted playsInline
+/>  
+        <button>Buy</button>
+        <p>From $1599 or $133 for 12 months</p>
+    </section>
   )
 }
 
