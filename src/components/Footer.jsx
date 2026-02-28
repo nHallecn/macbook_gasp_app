af6a814d357055a52f5ce3034cbfeb268605d2cc
NJI HALLE CHO-NKWENTI <njihalle10@gmail.com>
2026-02-28 12:06:23 +0100

    Revamp Footer component with new layout and links
    Updated Footer component to include additional information and footer links.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,27 @@
-
+import {footerLinks} from "../constants/index.js";
 
 const Footer = () => {
-  return (
-    <footer>
-      <p>&copy; 2024 MacBook GASP. All rights reserved.</p>
-    </footer>
-  );
+    return (
+        <footer>
+            <div className="info">
+                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
+                <img src="/logo.svg" alt ="Apple logo"/>
+            </div>
+
+            <hr />
+
+            <div className="links">
+                <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+                <ul>
+                    {footerLinks.map(({label, link }) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </footer>
+    )
 }
-export default Footer;
+export default Footer
